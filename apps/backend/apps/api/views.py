d6e3bd4bd17b764d3bb194f567e6128929803ebb
Zinchenko Ihor <zinchenkoihor757@gmail.com>
2026-04-29 09:29:02 +0000

    Add full k8s + docker setup (backend, frontend, ingress)

---
+++ apps/backend/apps/api/views.py
@@ -0,0 +1,6 @@
+from django.http import JsonResponse
+
+def hello(request):
+    return JsonResponse({
+        "message": "Hello from Django 🚀"
+    })
---
+++ apps/frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="padding: 40px">
+    <h1>DevOps Demo</h1>
+
+    <button @click="load">Get message</button>
+
+    <h2 v-if="msg">{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: ""
+    }
+  },
+
+  methods: {
+    async load() {
+      const res = await fetch("/api/hello/")
+      const data = await res.json()
+      this.msg = data.message
+    }
+  }
+}
+</script>
